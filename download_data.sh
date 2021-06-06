2278fb63c193bb1551f6d5f3e7ec04efea54b534
naveenmysore <navimn1991@gmail.com>
2021-06-06 13:46:19 -0700

    clean up

--- download_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-i=0
-while [ $i -ne 5 ]
-do
-        i=$(($i+1))
-        echo "$i"
-done
-curl --url https://downloads.psl.noaa.gov/Datasets/ncep.reanalysis/surface/slp.1948.nc --output /Volumes/ext_data/ncep/slp.1948.nc
--- geo_data_loader.py
+++ geo_data_loader.py
@@ -1,11 +1,13 @@
+import time
 import datetime
 import logging
 import pandas as pd
 from netCDF4 import Dataset
 import geopandas
+import matplotlib.pyplot as plt
 
 logging.basicConfig(format='%(levelname)s:%(message)s')
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.INFO)
 
 class GeoField:
     """
@@ -14,61 +16,78 @@ class GeoField:
     latitude) and one temporal dimension.  Optionally the fields may
     have a height dimension.
     """
-    def __init__(self, netcd4_loc, year, variable):
+    def __init__(self, netcd4_loc):
         """
         Initialize to empty data.
         """
         self.netCDF4 = netcd4_loc
-        self.start_date_time = datetime.datetime(year, 1, 1, 0, 0, 0)
         self.cdf_data = Dataset(self.netCDF4)
-        self.variable = variable
-        self.lons = None
-        self.lats = None
-        self.tm = None
+        self.lons = self.cdf_data.variables['lon'][:]
+        self.lats = self.cdf_data.variables['lat'][:]
+        self.tm = self.cdf_data.variables['time'][:]
         self.date_time = []
         self.cos_weights = None
+
+    def __repr__(self):
+        print(self.cdf_data)
+        return ""
 
     def get_cdf_data(self):
         return self.cdf_data
 
-    def data(self):
-        """
-        Return a copy of the stored data as a multi-array. If access
-        without copying is required for performance reasons, use
-        self.d at your own risk.
-        """
-        return self.cdf_data.copy()
 
-    def print_data_description(self):
-        d = self.cdf_data
-        print(d.title)
-        print('Description')
-        print(d.description)
-        print(f"\nDimension :")
-        print(f"    time: {d.dimensions['time']}")
-        print(f"    longitude: {d.dimensions['lon']}")
-        print(d.variables[self.variable])
-        print(f"    latitude: {d.dimensions['lat']}")
+class MonthlyMean(GeoField):
+    def __init__(self, netcd4_loc, start_year=1948, end_year=1949):
+        GeoField.__init__(self, netcd4_loc)
+        total_months = (end_year - start_year) * 12
+        dti = pd.date_range(f"{start_year}-01-01", periods=total_months, freq="M")
+        print(dti)
 
-    def load(self):
-        """
-        Load GeoData structure from netCDF file.
-        """
-        # extract spatial & temporal info
-        self.lons = self.cdf_data.variables['lon'][:]
-        self.lats = self.cdf_data.variables['lat'][:]
-        self.tm = self.cdf_data.variables['time'][:]
-        date_time = self.start_date_time
+    def __repr__(self):
+        print(self.cdf_data)
+        return ""
+
+    def get_data(self, variable):
+        return self.cdf_data[variable]
+
+
+class Daily4x(GeoField):
+    def __init__(self, netcd4_loc, year, variable):
+        GeoField.__init__(self, netcd4_loc)
+        self.date_time = []
+        date_time = datetime.datetime(year, 1, 1, 0, 0, 0)
         for _ in enumerate(self.tm):
             self.date_time.append(date_time)
             date_time += datetime.timedelta(hours=6)
-        logging.info(f"Loaded {self.variable} of shape {self.cdf_data.variables[self.variable].shape}")
+        logging.info(f"Loaded {variable} of shape {self.cdf_data.variables[variable].shape}")
+
+    def render(self, variable, time_slice=0):
+        """
+        Render a geographic plot
+        """
+        t = time.process_time()
+        render_data = dict()
+        render_data['Data'] = []
+        render_data['Latitude'] = []
+        render_data['Longitude'] = []
+        _data = self.cdf_data.variables
+        for i, latitude in enumerate(_data['lat'][:]):
+            for j, longitude in enumerate(_data['lon'][:]):
+                render_data['Longitude'].append(longitude)
+                render_data['Latitude'].append(latitude)
+                render_data['Data'].append(_data[variable][time_slice][i][j])
+        df = pd.DataFrame(render_data)
+        gdf = geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df.Longitude, df.Latitude))
+        world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+        ax = world.plot()
+        gdf.plot(column='Data', ax=ax, legend=True)
+        logging.info(f'Elapsed time {time.process_time() - t}')
+        plt.show()
 
 
 def main():
     logging.info("Testing GeoField")
-    gf = GeoField(netcd4_loc='/Volumes/ext_data/ncep/air.sig995.1948.nc', year=1948, variable='air')
-    gf.load()
+    gf = Daily4x(netcd4_loc='/Volumes/ext_data/ncep/air.sig995.1948.nc', year=1948, variable='air')
 
 
 if __name__ == "__main__":
